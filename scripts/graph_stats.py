d37aa6d1615f4bdfc81d3a75136ec23bd2cc9087
IdleBear <idlebear@redmaples.ca>
2022-04-18 15:53:13 -0400

    prety graphs

--- scripts/graph_stats.py
+++ scripts/graph_stats.py
@@ -13,7 +13,7 @@ mpl.use('pdf')
 
 
 # width as measured in inkscape
-width = 3.487
+width = 8  # 3.487
 height = width / 1.5
 
 
@@ -22,13 +22,19 @@ def parse_state_data(files):
     sb.set_theme(style="darkgrid")
     sb.set()
 
-    col = XKCD_ColourPicker()
+    colours = XKCD_ColourPicker()
     cols = []
-    cols.extend(col.values(1, "adobe"))
-    cols.extend(col.values(1, "darkish pink"))
-    cols.extend(col.values(1, "straw"))
-    cols.extend(col.values(1, "rich blue"))
-    cols.extend(col.values(1, "olive green"))
+    cols.extend(colours.values(1, "adobe"))
+    cols.extend(colours.values(1, "darkish pink"))
+    cols.extend(colours.values(1, "straw"))
+    cols.extend(colours.values(1, "rich blue"))
+    cols.extend(colours.values(1, "olive green"))
+
+    colours_policy = colours.values(2)
+
+    colours_lambda = [
+        colours.values(6), colours.values(9),
+    ]
 
     plt.rc('font', family='serif', serif='Times')
     plt.rc('text', usetex=True)
@@ -40,25 +46,55 @@ def parse_state_data(files):
 
     for f in files:
         df = pd.read_csv(f)
+        try:
+            df['cost-exponent'] = df['cost_exponent']
+        except KeyError:
+            pass
         df_list.append(df)
 
-    for l, h, label in [(0, 0.5, 'low'), (0.5, 1.1, 'high')]:
-        for col in ['avg-wait-time', 'max-wait-time', 'total-travel-distance', 'avg-task-dist', 'wait-sd', 'sim-time']:
+    df = pd.concat(df_list, ignore_index=True, sort=False)
+
+    # for l, h, label in [(0, 0.5, 'low'), (0.5, 20, 'high')]:
+    for l, h, label in [(0, 0.5, 'low'), (0.5, 20, 'high')]:
+        for col in ['avg-wait-time', 'max-wait-time', 'total-travel-distance', 'avg-task-dist']:
             colour_index = 0
             fig, ax = plt.subplots()
             fig.subplots_adjust(left=.15, bottom=.16, right=.99, top=.97)
 
-            for df in df_list:
-                colour_index += 1
-                df_slice = df[(df['lambda'] >= l) * (df['lambda'] <= h)]
-                sb.lineplot(x='lambda', y=col, hue='policy', data=df_slice, palette=[cols[colour_index]], linewidth=2.5)
+            # for df in df_list:
+            df_slice = df[(df['lambda'] >= l) * (df['lambda'] <= h)]
+            # df_slice = df_slice[(df_slice['cost-exponent'] >= -1) * (df_slice['cost-exponent'] <= 10)]
+            n = len(set(df_slice['cost-exponent']))
+            colour_list = colours.values(n)
+            sb.lineplot(x='lambda', y=col, hue='cost-exponent', data=df_slice, palette=colour_list, linewidth=2.5)
 
             ax.set_xlabel("Lambda")
             # ax.set_ylabel("Speed (m/s)")
             handles, labels = ax.get_legend_handles_labels()
-            ax.legend(handles=handles[0:], labels=labels[0:])
+            ax.legend(handles=handles[0:], labels=labels[0:], loc='upper left', title='Cost Exponent (p)')
             fig.set_size_inches(width, height)
-            fig.savefig('plot_{}_{}.pdf'.format(col, label))
+            fig.savefig('plot_lamda_{}_{}.pdf'.format(col, label))
+
+    for l, h, label in [(0, 0.5, 'low'), (0.5, 20, 'high')]:
+        for col in ['avg-wait-time', 'max-wait-time', 'total-travel-distance', 'avg-task-dist']:
+            colour_index = 0
+            fig, ax = plt.subplots()
+            fig.subplots_adjust(left=.15, bottom=.16, right=.99, top=.97)
+
+            # sb.lineplot(x='lambda', y=col, hue='cost-exponent', data=df_slice, palette=colour_list, linewidth=2.5)
+
+            df_slice = df[(df['lambda'] >= l) * (df['lambda'] <= h)]
+            # df_slice = df_slice[(df_slice['cost-exponent'] >= -1) * (df_slice['cost-exponent'] <= 10)]
+            n = len(set(df_slice['lambda']))
+            colour_list = colours.values(n)
+            sb.lineplot(x='cost-exponent', y=col, hue='lambda', data=df_slice, palette=colour_list, linewidth=2.5)
+
+            ax.set_xlabel("Cost Exponent")
+            # ax.set_ylabel("Speed (m/s)")
+            handles, labels = ax.get_legend_handles_labels()
+            ax.legend(handles=handles[0:], labels=labels[0:], title='Lambda')
+            fig.set_size_inches(width, height)
+            fig.savefig('plot_cost_{}_{}.pdf'.format(col, label))
 
 
 if __name__ == "__main__":
