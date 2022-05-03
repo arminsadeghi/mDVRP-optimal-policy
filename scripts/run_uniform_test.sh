36920e642f30ce626184971a2e6ec874b61197bf
IdleBear <idlebear@redmaples.ca>
2022-05-02 20:20:25 -0400

    scripts to automate tests

---
+++ scripts/run_uniform_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+costs=("1.0" "1.25" "1.5" "1.75" "2.0" "2.5" "3.0" "4.0" "5.0")
+
+prefix_str=""
+if [[ $1 ]]
+then
+    prefix_str="--prefix=$1"
+fi
+
+python main.py --multipass --cost-exponent=-2 --max-tasks=1000 --policy=batch_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
+python main.py --multipass --cost-exponent=-1 --max-tasks=1000 --policy=tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
+for i in ${costs[*]}; do
+    python main.py --multipass --cost-exponent=$i --max-tasks=1000 --policy=quad_wait_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
+done
