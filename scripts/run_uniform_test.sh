0f26d5a2820b4edc1913b5e33cef039f8564e57c
IdleBear <idlebear@redmaples.ca>
2022-05-13 17:16:27 -0400

    organised test setup Note that weights aren't a parameter (yet)

--- scripts/run_uniform_test.sh
+++ scripts/run_uniform_test.sh
@@ -7,6 +7,7 @@ then
     prefix_str="--prefix=$1"
 fi
 
+python main.py --multipass --cost-exponent=-3 --max-tasks=1000 --policy=weighted_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
 python main.py --multipass --cost-exponent=-2 --max-tasks=1000 --policy=batch_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
 python main.py --multipass --cost-exponent=-1 --max-tasks=1000 --policy=tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
 for i in ${costs[*]}; do
