063f23eb88ecf6fe769f332d12b15fdb1570c415
IdleBear <idlebear@redmaples.ca>
2022-07-18 10:28:09 -0400

    clean up graphing

--- scripts/graph_stats.py
+++ scripts/graph_stats.py
@@ -31,9 +31,9 @@ def parse_state_data(files, prefix):
 
     plt.rc('font', family='serif', serif='Times')
     plt.rc('text', usetex=True)
-    plt.rc('xtick', labelsize=8)
-    plt.rc('ytick', labelsize=8)
-    plt.rc('axes', labelsize=8)
+    plt.rc('xtick', labelsize=12)
+    plt.rc('ytick', labelsize=12)
+    plt.rc('axes', labelsize=12)
 
     df_list = []
 
@@ -63,66 +63,111 @@ def parse_state_data(files, prefix):
     ]
 
     ces = set(df['cost-exponent'])
-    df_d = df.loc[df['cost-exponent'] == 1.0]
+    policies = set(df['policy'])
+    # df_d = df.loc[df['cost-exponent'] == 1.0]
     for ce in ces:
         df_n = df.loc[df['cost-exponent'] == ce]
-        df.loc[df['cost-exponent'] == ce, 'avg-ratio'] = df_n['avg-wait-time'].to_numpy() / df_d['avg-wait-time'].to_numpy()
-        df.loc[df['cost-exponent'] == ce, 'max-ratio'] = df_n['max-wait-time'].to_numpy() / df_d['max-wait-time'].to_numpy()
-        df.loc[df['cost-exponent'] == ce, 'dist-ratio'] = df_n['total-travel-distance'].to_numpy() / df_d['total-travel-distance'].to_numpy()
+        # df.loc[df['cost-exponent'] == ce, 'avg-ratio'] = df_n['avg-wait-time'].to_numpy() / df_d['avg-wait-time'].to_numpy()
+        # df.loc[df['cost-exponent'] == ce, 'max-ratio'] = df_n['max-wait-time'].to_numpy() / df_d['max-wait-time'].to_numpy()
+        # df.loc[df['cost-exponent'] == ce, 'dist-ratio'] = df_n['total-travel-distance'].to_numpy() / df_d['total-travel-distance'].to_numpy()
+
+        if float(ce) < 1:
+            disp_postfix = " DIST"
+        else:
+            disp_postfix = " $\gamma$ = "+str(ce)
+
+        for policy in policies:
+            if policy.startswith('batch'):
+                disp_prefix = 'BATCH'
+            elif policy.startswith('tsp'):
+                disp_prefix = 'CR'
+            else:
+                disp_prefix = 'CR'
+
+            df.loc[np.bitwise_and(df['policy'] == policy, df['cost-exponent'] == ce), 'display-name'] = disp_prefix + disp_postfix
 
     # set the graph separations
-    # graphs = [(0, 0.5, 'low'), (0.5, 20, 'high')]
-    graphs = [(0.5, 2.1, 'high')]
+    # graphs = [(0, 0.5, 'low'), (0.5, 1.0, 'high')]
+    graphs = [(0.7, 0.8, 'high')]
+
+    colours = ['#fa8888', '#ca1111', '#df6699', '#5555fa', '#0000ff', '#6662aa', '#11fc11', '#ffaa21']
 
     # plot vs cost exponent
     # df = df[(df['cost-exponent'] >= 1) * (df['cost-exponent'] <= 3)]
-    n = len(set(df['cost-exponent']))
+    n = len(set(df['display-name']))
     colour_list = distinctipy.get_colors(n)  # colours.values(n)
     for l, h, label in graphs:
-        for col in ['avg-wait-time', 'max-wait-time', 'total-travel-distance', 'avg-task-dist', 'avg-and-max-wait', 'avg-ratio', 'max-ratio', 'dist-ratio']:
+        # , 'avg-and-max-wait', 'avg-ratio', 'max-ratio', 'dist-ratio']:
+        for col in ['avg-wait-time', 'max-wait-time']:
             colour_index = 0
             fig, ax = plt.subplots()
             fig.subplots_adjust(left=.15, bottom=.16, right=.99, top=.97)
 
             # for df in df_list:
             df_slice = df[(df['lambda'] >= l) * (df['lambda'] <= h)]
-            sb.lineplot(x='lambda', y=col, hue='cost-exponent', data=df_slice, palette=colour_list, linewidth=2.5)
+            sb.lineplot(x='lambda', y=col, hue='display-name', data=df_slice, palette=colours, linewidth=2.5)
 
-            ax.set_xlabel("Lambda")
-            # ax.set_ylabel("Speed (m/s)")
+            ax.set_xlabel("$\lambda$")
+            ax.set_ylabel("Time (s)")
             handles, labels = ax.get_legend_handles_labels()
-            for i in range(len(labels)):
-                if labels[i] == '-1.0':
-                    labels[i] = 'tsp'
-                if labels[i] == '-2.0':
-                    labels[i] = 'batch'
-                if labels[i] == '-3.0':
-                    labels[i] = '80/20 W'
-                if labels[i] == '-4.0':
-                    labels[i] = '50/50 W'
+            # for i in range(len(labels)):
+            #     if labels[i] == '-1.0':
+            #         labels[i] = 'tsp'
+            #     if labels[i] == '-2.0':
+            #         labels[i] = 'batch'
+            #     if labels[i] == '-3.0':
+            #         labels[i] = '80/20 W'
+            #     if labels[i] == '-4.0':
+            #         labels[i] = '50/50 W'
 
             ax.legend(handles=handles, labels=labels, loc='upper left', title='Method/Exponent')
             fig.set_size_inches(width, height)
             fig.savefig('{}plot_lamda_{}_{}.pdf'.format(prefix, col, label))
 
-    # plot vs Lambda
-    for l, h, label in graphs:
-        n = len(set(df[(df['lambda'] >= l) * (df['lambda'] <= h)]['lambda']))
-        colour_list = distinctipy.get_colors(n)  # colours.values(n)
-        for col in ['avg-wait-time', 'max-wait-time', 'total-travel-distance', 'avg-task-dist', 'avg-and-max-wait', 'avg-ratio', 'max-ratio']:
+        for col in ['total-travel-distance', 'avg-task-dist']:
             colour_index = 0
             fig, ax = plt.subplots()
             fig.subplots_adjust(left=.15, bottom=.16, right=.99, top=.97)
 
+            # for df in df_list:
             df_slice = df[(df['lambda'] >= l) * (df['lambda'] <= h)]
-            sb.lineplot(x='cost-exponent', y=col, hue='lambda', data=df_slice, palette=colour_list, linewidth=2.5)
+            sb.lineplot(x='lambda', y=col, hue='display-name', data=df_slice, palette=colours, linewidth=2.5)
 
-            ax.set_xlabel("Cost Exponent")
-            # ax.set_ylabel("Speed (m/s)")
+            ax.set_xlabel("$\lambda$")
+            ax.set_ylabel("Distance (m)")
             handles, labels = ax.get_legend_handles_labels()
-            ax.legend(handles=handles[0:], labels=labels[0:], title='Lambda')
+            # for i in range(len(labels)):
+            #     if labels[i] == '-1.0':
+            #         labels[i] = 'tsp'
+            #     if labels[i] == '-2.0':
+            #         labels[i] = 'batch'
+            #     if labels[i] == '-3.0':
+            #         labels[i] = '80/20 W'
+            #     if labels[i] == '-4.0':
+            #         labels[i] = '50/50 W'
+
+            ax.legend(handles=handles, labels=labels, loc='upper left', title='Method/Exponent')
             fig.set_size_inches(width, height)
-            fig.savefig('{}plot_cost_{}_{}.pdf'.format(prefix, col, label))
+            fig.savefig('{}plot_lamda_{}_{}.pdf'.format(prefix, col, label))
+
+    # # plot vs Lambda
+    # for l, h, label in graphs:
+    #     n = len(set(df[(df['lambda'] >= l) * (df['lambda'] <= h)]['lambda']))
+    #     colour_list = distinctipy.get_colors(n)  # colours.values(n)
+    #     for col in ['avg-wait-time', 'max-wait-time', 'total-travel-distance', 'avg-task-dist', 'avg-and-max-wait', 'avg-ratio', 'max-ratio']:
+    #         colour_index = 0
+    #         fig, ax = plt.subplots()
+    #         fig.subplots_adjust(left=.15, bottom=.16, right=.99, top=.97)
+
+    #         df_slice = df[(df['lambda'] >= l) * (df['lambda'] <= h)]
+    #         sb.lineplot(x='cost-exponent', y=col, hue='lambda', data=df_slice, palette=colour_list, linewidth=2.5)
+
+    #         ax.set_xlabel("Cost Exponent")
+    #         # ax.set_ylabel("Speed (m/s)")
+    #         handles, labels = ax.get_legend_handles_labels()
+    #         ax.legend(handles=handles[0:], labels=labels[0:], title='Lambda')
+    #         fig.set_size_inches(width, height)
+    #         fig.savefig('{}plot_cost_{}_{}.pdf'.format(prefix, col, label))
 
 
 if __name__ == "__main__":
