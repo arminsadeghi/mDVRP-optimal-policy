ef01c6315d19715ee39da3bbbbf02f597f062e65
IdleBear <idlebear@redmaples.ca>
2022-10-07 14:56:19 -0400

    slight mod on run script

--- scripts/run_batch_eta_tsp.sh
+++ scripts/run_batch_eta_tsp.sh
@@ -9,8 +9,8 @@ then
 fi
 
 for a in ${etas[*]}; do
-    python main.py --multipass --cost-exponent=-2.0 --eta=$a --max-tasks=1000 --policy=batch_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
+    python main.py --multipass --cost-exponent=-2.0 --eta=$a --max-tasks=1000 --policy=lkh_batch_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
     for c in ${costs[*]}; do
-        python main.py --multipass --cost-exponent=$c --eta=$a --max-tasks=1000 --policy=batch_wait_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
+        python main.py --multipass --cost-exponent=$c --eta=$a --max-tasks=1000 --policy=lkh_batch_wait_tsp $prefix_str --service-time 1 --generator uniform  > /dev/null 2>&1 &
     done
 done
